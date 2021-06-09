b72d65fc30d1ab6df70334c51d37f20d1f76a7d5
greydonbuckley <51009262+greydonbuckley@users.noreply.github.com>
2021-06-09 13:33:44 -0700

    Add artifact registration in a branch

--- vars/printBuildinfo.groovy
+++ vars/printBuildinfo.groovy
@@ -12,5 +12,7 @@ def call(body) {
     if (env.param1 == 'One default') {
         echo "Param1 is default"
     }
+    snDevOpsArtifact(artifactsPayload: """{"artifacts": [{"name": "shared-lib.jar", "version": "1.1","semanticVersion": "1.1.1","repositoryName": "fakerepo"}], "branchName": "master"}""")
+   
     return this
 }
