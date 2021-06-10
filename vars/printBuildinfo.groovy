25eaee3992600fcfade64049fb424acad68e6057
greydonbuckley <51009262+greydonbuckley@users.noreply.github.com>
2021-06-10 14:10:46 -0700

    Update printBuildinfo.groovy

--- vars/printBuildinfo.groovy
+++ vars/printBuildinfo.groovy
@@ -12,7 +12,7 @@ def call(body) {
     if (env.param1 == 'One default') {
         echo "Param1 is default"
     }
-    snDevOpsArtifact(artifactsPayload: """{"artifacts": [{"name": "shared-lib.jar", "version": "1.1","semanticVersion": "1.1.1","repositoryName": "fakerepo"}], "branchName": "master"}""")
+    snDevOpsArtifact(artifactsPayload: """{"artifacts": [{"name": "zibbuild-web.jar", "version": "1.9","semanticVersion": "1.9.0","repositoryName": "services-1031"}], "branchName": "master"}""")
    
     return this
 }
