b00aa623f0e6b42e97ddd64a06f301594cad50ef
greydonbuckley <51009262+greydonbuckley@users.noreply.github.com>
2021-06-11 08:18:26 -0700

    add back closure

--- vars/storeArtifact.groovy
+++ vars/storeArtifact.groovy
@@ -2,14 +2,13 @@
 def call(Closure body) {
     // evaluate the body block, and collect configuration into the object
     def config = [:]
-//    body.resolveStrategy = Closure.DELEGATE_FIRST
-//    body.delegate = config
-//	body()
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+	body()
 	println(config)
   
-		def url = java.net.URLDecoder.decode(env.JOB_BASE_NAME,"UTF-8");
-		println("Uploading: ${url}")
- 		snDevOpsArtifact(artifactsPayload: """{"artifacts": [{"name": "zibbuild-web.jar", "version": "1.9","semanticVersion": "1.9.0","repositoryName": "services-1031"}]}""")
- 		echo ">>>>>>>Artifact Registered in Snow!"
-		
+    def url = java.net.URLDecoder.decode(env.JOB_BASE_NAME,"UTF-8");
+    println("Uploading: ${url}")
+    snDevOpsArtifact(artifactsPayload: """{"artifacts": [{"name": "zibbuild-web.jar", "version": "1.9","semanticVersion": "1.9.0","repositoryName": "services-1031"}]}""")
+    echo ">>>>>>>Artifact Registered in Snow!"
 }
